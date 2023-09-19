3f32f7770c3cc0f5520e6d5012d0d687ae4f85b1
mosesmramba <moses.mramba@student.moringaschool.com>
2023-09-19 11:58:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue){
+  if(someValue<42){
+    return(42-someValue);
+  }
+  else{
+    return(someValue-42);
+  }
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+  
+}
+ 
+function distanceTravelledInFeet(start,destination){
+  if(start>destination){
+    return((start-destination)*264);
+  }
+  else{
+    return((destination-start)*264);
+  }
+}
+
+function calculatesFarePrice(start,destination){
+  const feet=distanceTravelledInFeet(start,destination)
+   if (feet<=400){
+    return 0;
+   }
+   else if(
+    feet>=400 && feet<=2000){
+      return((feet-400)*0.02);
+   }
+   else if(feet>=2000 && feet<=2500){
+    return 25;
+   }
+   else if(feet>2500){
+    return "cannot travel that far";
+   }
+
+}
+  
+
